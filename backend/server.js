6357335ddf3e000c3cf31cbf50d4b4add935fdb1
sithulimethasha-arch <sithuli.methasha@gmail.com>
2026-04-28 09:33:46 +0530

    Fix Railway port

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/items", itemRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 mongoose
   .connect(process.env.MONGO_URI)
